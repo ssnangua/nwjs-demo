08bc0261f9bcc00ce140d776ed8a2940a34801ea
ssnangua <kainanhong@gmail.com>
2024-06-03 17:34:19 +0800

    Update filePicker

--- #3_API/demo/demo/filePicker.js
+++ #3_API/demo/demo/filePicker.js
@@ -9,22 +9,28 @@ function setAttribute(attr, value) {
   else $input.setAttribute(attr, value);
 }
 
+const path = require("node:path");
+const START_IN = {};
+["desktop", "documents", "downloads", "music", "pictures", "videos"].forEach((startIn) => {
+  START_IN[startIn] = path.join(process.env.USERPROFILE, startIn);
+});
+
 export function showFilePicker({
   type = "open",
-  filename = "",
+  suggestedName = "",
   multiple = false,
   accept = "",
   openDirectory = false,
-  title = "",
-  defaultPath = "",
+  openDirectoryDesc = "",
+  startIn = "",
 }) {
   return new Promise((resolve, reject) => {
-    setAttribute("nwsaveas", type === "save" ? filename : false);
+    setAttribute("nwsaveas", type === "save" ? suggestedName : false);
     setAttribute("multiple", multiple);
     setAttribute("accept", accept);
     setAttribute("nwdirectory", openDirectory);
-    setAttribute("nwdirectorydesc", title);
-    setAttribute("nwworkingdir", defaultPath);
+    setAttribute("nwdirectorydesc", openDirectoryDesc);
+    setAttribute("nwworkingdir", START_IN[startIn] || startIn);
     $input.onchange = () => {
       resolve([...$input.files]);
       $input.value = "";
@@ -34,12 +40,16 @@ export function showFilePicker({
   });
 }
 
-export function showOpenDialog(options = {}) {
-  return showFilePicker({ ...options, type: "open" });
+export function showOpenFilePicker({ multiple, accept, startIn } = {}) {
+  return showFilePicker({ type: "open", multiple, accept, startIn });
 }
 
-export function showSaveDialog(options = {}) {
-  return showFilePicker({ ...options, type: "save" });
+export function showSaveFilePicker({ suggestedName, accept, startIn } = {}) {
+  return showFilePicker({ type: "save", suggestedName, accept, startIn });
 }
 
-export default { showFilePicker, showOpenDialog, showSaveDialog };
+export function showDirectoryPicker({ openDirectoryDesc, startIn } = {}) {
+  return showFilePicker({ type: "open", openDirectory: true, openDirectoryDesc, startIn });
+}
+
+export default { showFilePicker, showOpenFilePicker, showSaveFilePicker, showDirectoryPicker };
